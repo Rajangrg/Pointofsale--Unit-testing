0d8bbb0d0d74be6f5af8b8407ade0a301b993566
Rajan G <rajangrg1@hotmail.com>
2020-03-11 12:26:41 +1300

    Nunit installed and GetProductName passed

---
+++ POS.Library.Tests/ProductTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Library.Tests
+{
+    [TestFixture]
+    public class ProductTest
+    {
+        [Test]
+        public void GetProductName()
+        {
+            //Arrange
+            Product product = new Product();
+
+            //Act
+            product.SetProductName("A");
+            string result = product.GetProductName();
+
+            //Assert
+            Assert.AreEqual("A", result);
+        }
+    }
+}
---
+++ POS.Library/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Library
+{
+    public class Product
+    {
+        protected string productName;
+
+
+        public void SetProductName(string productName)
+        {
+            this.productName = productName;
+        }
+
+        public string GetProductName()
+        {
+            return productName;
+        }
+    }
+}
