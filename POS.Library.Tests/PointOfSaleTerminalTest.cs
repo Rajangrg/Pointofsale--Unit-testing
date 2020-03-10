21db704e7fc2a62f47a08f67fc63bf1aa3fca39c
Rajan G <rajangrg1@hotmail.com>
2020-03-11 12:57:19 +1300

    pointOfSaleTerminal created and noItem in terminal passed

---
+++ POS.Library.Tests/PointOfSaleTerminalTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Library.Tests
+{
+    [TestFixture]
+    public class PointOfSaleTerminalTest
+    {
+
+        [Test]
+        public void NoItemIsAddedOnPointOfSaleTerminal()
+        {
+            //Arrange
+            PointOfSaleTerminal pointOfSale = new PointOfSaleTerminal();
+
+            //Act
+            int result = pointOfSale.ItemCount();
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ POS.Library/PointOfSaleTerminal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Library
+{
+    public class PointOfSaleTerminal
+    {
+        protected List<Product> items; //for stroing items 
+
+        //constructor
+        public PointOfSaleTerminal()
+        {
+           items = new List<Product>();
+        }
+
+        public void ScanProduct(Product item)
+        {
+            this.items.Add(item);
+        }
+
+        public int ItemCount()
+        {
+            return items.Count(); //count all the items from the lists
+        }
+
+
+    }
+}
