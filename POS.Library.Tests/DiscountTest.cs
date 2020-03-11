4661a8af03e8c536d3ad355bd12735675fc9d825
Rajan G <rajangrg1@hotmail.com>
2020-03-11 14:55:32 +1300

    Discount - 3 pack discount is calculated and passed

--- POS.Library.Tests/DiscountTest.cs
+++ POS.Library.Tests/DiscountTest.cs
@@ -10,8 +10,8 @@ namespace POS.Library.Tests
     [TestFixture]
     public class DiscountTest
     {
-        [Test] //discount on pack
-        public void GetTotalValueForSixSameProduct()
+        [Test] //discount on six-pack
+        public void GetTotalValueForSixSameProductC()
         {
             //Arrange
             
@@ -27,6 +27,56 @@ namespace POS.Library.Tests
 
             Assert.AreEqual(5, pointOfSale.DiscountForC("C"));
             //Assert.AreEqual(10, pointOfSale.DiscountForC("C"));
+        }
+
+        [Test] 
+        public void GetTotalValueForfourSameProductC()
+        {
+            //Arrange
+            PointOfSaleTerminal pointOfSale = new PointOfSaleTerminal();
+            Product product = new Product("C", 5, 1);
+
+            //Act
+            pointOfSale.ScanProduct(product);
+
+            //Assert
+            Assert.AreEqual(5, pointOfSale.DiscountForC("C"));
+        }
+
+
+        [Test] //discount on three when bought together
+        public void GetTotalValueFoThreeSameProductA()
+        {
+            //Arrange
+
+            PointOfSaleTerminal pointOfSale = new PointOfSaleTerminal();
+            Product product = new Product("A", 3, 1.25);
+
+            //Act
+            pointOfSale.ScanProduct(product);
+
+            //Assert
+
+            Assert.AreEqual(3, pointOfSale.DiscountForA("A"));
+           
+        }
+
+
+        [Test] //
+        public void GetTotalValueFoTwoSameProductA()
+        {
+            //Arrange
+
+            PointOfSaleTerminal pointOfSale = new PointOfSaleTerminal();
+            Product product = new Product("A", 2, 1.25);
+
+            //Act
+            pointOfSale.ScanProduct(product);
+
+            //Assert
+
+            Assert.AreEqual(2.5, pointOfSale.DiscountForA("A"));
+
         }
 
     }
--- POS.Library/PointOfSaleTerminal.cs
+++ POS.Library/PointOfSaleTerminal.cs
@@ -8,16 +8,26 @@ namespace POS.Library
 {
     public class PointOfSaleTerminal
     {
-        public List<Product> items; //for stroing items 
+        public List<Product> items; //for stroing items or scanned items
         protected double totalValueBeforeDiscount;
 
 
-        //product C discount details -- refactor create seprate class
+        //product C discount details
+        //refactor later create seprate class for discount 
+
         protected int cProductQuantity;
         protected double totalPriceWithoutDiscountForC = 0;
-        protected double totalPriceForC = 0;
-        protected int perUnitPriceForC = 1;
+        protected double totalPriceForC = 0d;
+        protected double perUnitPriceForC = 1;
         protected double cDiscountPrice = 16.667; // in percentage
+
+        //product C discount details 
+        protected int aProductQuantity;
+        protected double totalPriceWithoutDiscountForA = 0;
+        protected double totalPriceForA = 0d;
+        protected double perUnitPriceForA = 1.25;
+        protected double aDiscountPrice = 25; // in percentage
+
 
 
         //constructor
@@ -48,7 +58,10 @@ namespace POS.Library
         }
 
 
-        // Discount  -- refactor later 
+
+
+        // ---------Discount---------
+        //refactor create seperate class 
 
         public double DiscountForC(string productName)
         {
@@ -62,25 +75,50 @@ namespace POS.Library
                         cProductQuantity += item.GetProductQuantity(); // from Product class
 
                     }
-                    totalPriceWithoutDiscountForC = cProductQuantity * 1;
+                    totalPriceWithoutDiscountForC = cProductQuantity * perUnitPriceForC;
 
                     if (cProductQuantity >= 6)
                     {
                         totalPriceForC = Math.Round((totalPriceWithoutDiscountForC) - ((totalPriceWithoutDiscountForC * cDiscountPrice) / 100));
-                        //totalPriceForC = (totalPriceForWithoutDiscountforC) - ((totalPriceForWithoutDiscountforC * cDscountPrice) / 100);
                     }
-               
                     else
                     {
                         totalPriceForC = totalPriceWithoutDiscountForC;
                     }
                 }
-
             }
 
-            return totalPriceForC; //total sum
+            return totalPriceForC; //total sum only for C
         }
 
+
+        public double DiscountForA(string productName)
+        {
+
+            foreach (var item in this.items)
+            {
+                if (item.GetProductName() == productName)
+                {
+                    for (int i = 0; i < ItemCount(); i++)
+                    {
+                        aProductQuantity += item.GetProductQuantity(); // from Product class
+
+                    }
+                    totalPriceWithoutDiscountForA = aProductQuantity * perUnitPriceForA;
+
+                    if (aProductQuantity >= 3)
+                    {
+                        totalPriceForA = Math.Round((totalPriceWithoutDiscountForA) - ((totalPriceWithoutDiscountForA * aDiscountPrice) / 100));       
+                    }
+                    else
+                    {
+                        totalPriceForA = totalPriceWithoutDiscountForA;
+                    }
+                }
+
+            }
+            return totalPriceForA; //total sum only for A
+        }
 
 
     }
